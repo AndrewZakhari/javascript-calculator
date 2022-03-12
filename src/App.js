6a3b81562250d994438367505f203408e9e6e83e
Andrew A <82312201+deijiii@users.noreply.github.com>
2022-03-12 03:02:44 +0200

    Done with Operations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import './App.css';
-import React from 'react';
-import Buttons from './calcButtons';
-import Calculations from './calculations';
+import React, { useState } from 'react';
 
 
 
+let arr = [0,1,2,3,4,5,6,7,8,9];
+let arr2 = ['=','+','-','*','/','.','AC',]
 export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-     result: '0', 
-    }
-  }      
   render(){
     return(
-      <div>
-        <div id="display">{this.state.result}</div>
-        <Buttons />
-        <Calculations />
+      <div>   
+         
+    <Calculator />
       </div>
+      
     )
   }
+}
+const Calculator = () => {
+  let [display, setDisplay] = useState(0); 
+ 
+
+ const updateState = e => {
+   if(display === 0){
+    setDisplay(e.target.value);
+   }else{
+  setDisplay(display + e.target.value ) 
+   }
+ }
+  const operations = e => {
+   let value = e.target.value;
+   let displayArr = [];
+   parseFloat(display);
+   if(value === '=') {
+     displayArr.push(display);
+     eval(displayArr);
+    setDisplay(eval(...displayArr));
+   }else if(value === '+'){
+    setDisplay(display + ' + ');    
+   }else if(value === '-'){
+     setDisplay(display + ' - ');
+   }else if(value === '*'){
+     setDisplay(display + ' * ');
+   }else if(value === '/'){
+     setDisplay(display + ' / ')
+   }else if(value === '.'){
+     let DecimalArr = display.split(' ')
+     if(DecimalArr.hasOwnProperty('.')){
+       setDisplay(display)
+     }else {
+     setDisplay(display + '.')
+     }
+   }else if(value === 'AC'){
+     setDisplay(0);
+   }
+  }
+  
+  return(
+    <>
+    <div id="display">{display}</div>
+    <div>
+    {arr.map(num => <button onClick={updateState} value={num} id={num}>{num}</button>)}
+    </div>
+    <div>
+    {arr2.map(i => <button onClick={operations} value={i} id={i}>{i}</button>)}
+    </div>
+    </>
+  )
 }
 
